719e0c07f9942c364e5ff3f40cc5bfa5ac385c38
Rivanildo <rivanildo.silva@dce.ufpb.br>
2021-07-05 10:26:01 -0300

    adicionando seção sobre

--- sobre.js
+++ sobre.js
@@ -12,9 +12,20 @@ export default function sobre(props) {
       <SafeAreaView style={styles.container}>
     
       <ScrollView style={styles.scrollView}>
-        <Text style={styles.text} onPress={() => Linking.openURL("https://www.google.com/")}>
+        
+        
+        <Text style={styles.text} >
           
-          
+        A Biblioteca Cidadã é um acervo digital, fruto de uma curadoria realizada por professores/as da Rede Estadual de Educação.  Composta por inúmeros títulos e mídias, a Biblioteca tem como objetivo disponibilizar conhecimento em diversos formatos, com a missão de contribuir para o desenvolvimento social, cultural e intelectual de todas as pessoas, por meio da democratização do acesso a obras gratuitas e/ou de domínio público.
+        </Text>
+        <Text style={styles.text} >
+A estrutura da curadoria foi pensada a partir do acesso a títulos divididos em áreas de conhecimento, com subdivisões (quando for o caso). Os títulos disponíveis para cada área foram escolhidos por meio de uma seleção com base nos seguintes critérios: afinidade com alguma das áreas de conhecimento apresentadas; pertinência e relevância, tanto para a educação escolar quanto para a formação pessoal; obra estabelecida como de domínio público ou que esteja com acesso gratuito, a fim de que não haja ônus para a pessoa interessada no seu conteúdo. 
+</Text>
+<Text style={styles.text} >
+Além dos títulos divididos por áreas de conhecimento, também foram disponibilizados sites de museus e observatórios, assim como listamos possibilidades de conhecimento nos mais diversos temas em outras mídias, tais como podcasts e audiolivros.
+</Text>
+<Text style={styles.text} >
+É fundamental destacar que toda a idealização da Biblioteca esteve comprometida com o respeito aos direitos humanos. Desse modo, buscamos possibilitar, por meio desse instrumento, a difusão de valores  como a cidadania, a liberdade, a solidariedade e a diversidade cultural, visando à promoção de uma sociedade mais acessível, equitativa e livre de preconceitos.
                
         </Text>
        
@@ -47,13 +58,17 @@ const styles = StyleSheet.create({
 
   
   text: {
-    fontSize: 80,
-    marginTop: 40,
+    textAlign:"justify",
+    fontSize: 20,
+    marginLeft:10,
+    marginRight:15,
+    marginBottom:10,
     color: "white",
 
   },
   scrollView:{
-    marginBottom:120,
+    marginTop:170,
+    marginBottom:110,
   },
 
 
